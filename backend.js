0a85db8c433e9a75bfebe6a7c19ec8f717df55f0
AkhilChandran <akhil@trycle.com>
2026-06-05 00:17:29 +0530

    data error handled

--- backend.js
+++ backend.js
@@ -1,4 +1,4 @@
-async function fetchWeather(event) {
+async function fetchWeather() {
   const getCity = document.getElementById("city");
 
   const city = getCity.value;
@@ -6,7 +6,26 @@ async function fetchWeather(event) {
   const response = await fetch(
     `http://api.weatherapi.com/v1/current.json?key=96d6132675004181892111830260406&q=${city}&aqi=no`,
   );
+
   const data = await response.json();
 
-  console.log(data);
+  if (data.error) {
+    const display = document.getElementById("display");
+    display.innerHTML = "";
+
+    const weatherData = document.createElement("p");
+
+    weatherData.textContent = "Enter valide city name";
+
+    display.appendChild(weatherData);
+  } else {
+    const display = document.getElementById("display");
+
+    display.innerHTML = "";
+    const weatherData = document.createElement("p");
+
+    weatherData.textContent = `Weather Condition: ${data.current.condition.text}`;
+
+    display.appendChild(weatherData);
+  }
 }
